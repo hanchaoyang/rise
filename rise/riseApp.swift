a5bae79d44dfa7318daddda4c3271ba3d027615f
Han Chaoyang <hanchaoyang1030@gmail.com>
2026-08-08 20:44:16 +0800

    feat: Initial project setup

---
+++ rise/riseApp.swift
@@ -0,0 +1,17 @@
+//
+//  riseApp.swift
+//  rise
+//
+//  Created by 韩朝阳 on 2026/8/8.
+//
+
+import SwiftUI
+
+@main
+struct riseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
